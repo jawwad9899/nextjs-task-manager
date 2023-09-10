8ba7e82178043a1b0c721426a637d0f78eae2481
jawwad9899 <janonymous9899@gmail.com>
2023-09-10 15:23:26 +0500

    updated!

--- app/api/user/current/route.ts
+++ app/api/user/current/route.ts
@@ -5,12 +5,17 @@ import verifyToken from "@/helper/verifyToken";
 export async function GET(request: NextRequest) {
   try {
     await connectDB();
-    const token: any = request.cookies.get("token")?.value;
-    // grab the user from token
+    const token: any = request.cookies.get("token")?.value || "";
+    if (token) {
+      // grab the user from token
+      const user = await verifyToken(token);
 
-    const user = await verifyToken(token);
-
-    return NextResponse.json({ success: true, user }, { status: 200 });
+      return NextResponse.json({ success: true, user }, { status: 200 });
+    }
+    return NextResponse.json(
+      { success: false, msg: "jwt token must be provided!" },
+      { status: 400 }
+    );
   } catch (error: any) {
     console.error(error);
     return NextResponse.json(
